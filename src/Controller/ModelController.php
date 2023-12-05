252c7d8a2c21f81da96d95f8ae0d941853d71363
Melissa Benard <benardmeli@yahoo.fr>
2023-12-05 16:18:19 +0100

    Update a model

--- src/Controller/ModelController.php
+++ src/Controller/ModelController.php
@@ -4,23 +4,74 @@ namespace App\Controller;
 
 use App\Entity\Model;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ModelType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/modeles')]
 class ModelController extends AbstractController
 {
     /**
      * Models
      */
-    #[Route('/modeles', name: 'app_models')]
-    public function models(EntityManagerInterface $em): Response
+    #[Route('/', name: 'app_models')]
+    public function models(EntityManagerInterface $em, Request $request): Response
     {
+        // Create a new model
+        $model = new Model(); 
+        $form = $this->createForm(ModelType::class, $model); 
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($model); 
+            $em->flush(); 
+
+            $this->addFlash(
+                'success',
+                'Nouveau modèle créé!'
+            );
+        }
+
         // Get all the models in the database 
-        $models = $em->getRepository(Model::class)->findAll();  
+        $models = $em->getRepository(Model::class)->findAll(); 
 
         return $this->render('model/models.html.twig', [
             'models' => $models, 
+            'add_form' => $form->createView(),
+        ]);
+    }
+
+        /**
+     * Detail of a model and update a model
+     */
+    #[Route('/{id}', name: 'app_model')]
+    public function model(EntityManagerInterface $em, Model $model = null, Request $request): Response
+    {
+        // Check if the model exists
+        if ($model == null) {
+            $this->addFlash(
+                'danger',
+                'Modèle introuvable!'
+            );
+            return $this->redirectToRoute('app_models'); 
+        } 
+ 
+        // Update form
+        $form = $this->createForm(ModelType::class, $model); 
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($model); 
+            $em->flush(); 
+            $this->addFlash(
+                'success',
+                'Le modèle a bien été modifié!'
+            );
+        }
+  
+        return $this->render('model/model.html.twig', [
+            'model' => $model, 
+            'update' => $form->createView(),
         ]);
     }
 }
