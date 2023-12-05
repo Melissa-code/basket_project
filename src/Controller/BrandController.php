5ebff2d42cea13c8fe02c0f85e6280cabb8f20ad
Melissa Benard <benardmeli@yahoo.fr>
2023-12-05 15:42:03 +0100

    Update a brand and display the details of a brand

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -10,12 +10,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/marques')]
 class BrandController extends AbstractController
 {
     /**
      * List of the brands & create form
      */
-    #[Route('/marques', name: 'app_brands')]
+    #[Route('/', name: 'app_brands')]
     public function brands(EntityManagerInterface $em, Request $request): Response
     {
         // Create a new brand
@@ -38,6 +39,39 @@ class BrandController extends AbstractController
         return $this->render('brand/brands.html.twig', [
             'brands' => $brands,
             'add_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Detail of a brand and update a brand
+     */
+    #[Route('/{id}', name: 'app_brand')]
+    public function brand(EntityManagerInterface $em, Brand $brand = null, Request $request): Response
+    {
+        // Check if the beand exists
+        if ($brand == null) {
+            $this->addFlash(
+                'danger',
+                'Marque introuvable!'
+            );
+            return $this->redirectToRoute('app_brands'); 
+        } 
+ 
+        // Update form
+        $form = $this->createForm(BrandType::class, $brand); 
+        $form->handleRequest($request); 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($brand); 
+            $em->flush(); 
+            $this->addFlash(
+                'success',
+                'La marque a bien été modifiée!'
+            );
+        }
+  
+        return $this->render('brand/brand.html.twig', [
+            'brand' => $brand, 
+            'update' => $form->createView(),
         ]);
     }
 }
